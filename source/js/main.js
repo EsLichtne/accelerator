019862d9419e54620402478fa51dbe82d9715cb8
Ксения <easyghoul@gmail.com>
2024-03-25 09:57:46 +0700

    feat: Инициализирует и оживляет слайдер жюри

--- source/js/main.js
+++ source/js/main.js
@@ -1,4 +1,28 @@
-// https://swiperjs.com/get-started#installation
-// import Swiper from "swiper";
-// import {Navigation, Pagination} from "swiper/modules";
-// import 'swiper/css';
+import Swiper from 'swiper';
+import {Navigation} from 'swiper/modules';
+
+new Swiper('.swiper', {
+  modules: [Navigation],
+  loop: true,
+  speed: 500,
+
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  breakpoints: {
+    320: {
+      slidesPerView: 1,
+      initialSlide: 2,
+    },
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 40,
+    },
+    1366: {
+      slidesPerView: 4,
+      spaceBetween: 40,
+    }
+  }
+});
