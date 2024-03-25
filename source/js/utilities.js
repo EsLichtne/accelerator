77083e0ddb9255de6e111349efcaf461cdff4688
Ксения <easyghoul@gmail.com>
2024-03-25 15:28:04 +0700

    feat: Создаёт функцию устранения дребезга

---
+++ source/js/utilities.js
@@ -0,0 +1,16 @@
+const throttle = (callback, delayBetweenFrames = 1000) => {
+  let timeoutId, lastCallTime;
+
+  return (...rest) => {
+    const elapsedTime = Date.now() - lastCallTime;
+    const delay = Math.max(delayBetweenFrames - elapsedTime, 0);
+
+    window.clearTimeout(timeoutId);
+    timeoutId = window.setTimeout(() => {
+      callback(...rest);
+      lastCallTime = Date.now();
+    }, delay);
+  };
+};
+
+export { throttle };
