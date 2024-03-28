73fa75d9b7aee7dacc0671f717d6933bca856f6a
Ксения <easyghoul@gmail.com>
2024-03-28 12:15:08 +0700

    feat: Добавляет функционал блока абонементов

---
+++ source/js/abonements/abonements.js
@@ -0,0 +1,40 @@
+import { renderTabs } from '../tabs/tabs.js';
+
+const tabsContainer = document.querySelector('.inline-tabs');
+const tabs = tabsContainer.querySelectorAll('.inline-tabs__tab');
+const abonements = document.querySelectorAll('.subscription-card');
+
+const prices = {
+  'price-tab-1': {
+    0: '5000',
+    1: '1700',
+    2: '2700',
+  },
+
+  'price-tab-2': {
+    0: '30 000',
+    1: '10 200',
+    2: '16 200',
+  },
+
+  'price-tab-3': {
+    0: '60 000',
+    1: '20 400',
+    2: '32 400',
+  },
+};
+
+renderTabs(tabsContainer, tabs);
+
+document.addEventListener('inline-tabs', (event) => {
+  abonements.forEach((abonement, index) => {
+    abonement.querySelector('.subscription-card__price').innerHTML =
+      `
+      <span class="subscription-card__shadow" aria-hidden="true">${prices[event.detail]?.[index]}</span>
+      <span class="visually-hidden">Стоимость (рублей):</span>
+      ${prices[event.detail]?.[index]}
+      `;
+  });
+});
+
+renderTabs(tabsContainer, tabs);
--- source/js/main.js
+++ source/js/main.js
@@ -2,3 +2,4 @@ import './slider/slider.js';
 import './video/video.js';
 import './upload/validation.js';
 import './accordion/accordion.js';
+import './abonements/abonements.js';
