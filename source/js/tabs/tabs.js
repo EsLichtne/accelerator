4625b8093fddcf17f65f338115d310fbf864ab68
Ксения <easyghoul@gmail.com>
2024-03-28 12:25:43 +0700

    feat: Переносит создание события внутрь условия

--- source/js/tabs/tabs.js
+++ source/js/tabs/tabs.js
@@ -4,9 +4,9 @@ const renderTabs = (container, tabs) => {
   container.addEventListener('click', throttle((event) => {
     const tabId = event.target.dataset.tabId;
 
-    document.dispatchEvent(new CustomEvent(`${container.id}`, { detail: tabId }));
-
     if (event.target && event.target.classList.contains(tabs[0].className)) {
+      document.dispatchEvent(new CustomEvent(`${container.id}`, { detail: tabId }));
+
       const selectedTab = event.target;
 
       tabs.forEach((tab) => {
