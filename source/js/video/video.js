9e7b51bf4befaf45cd2fb80363405333b1eff2bf
Ксения <easyghoul@gmail.com>
2024-04-01 10:57:44 +0700

    feat: Добавляет размеры тегу iframe

--- source/js/video/video.js
+++ source/js/video/video.js
@@ -1,11 +1,14 @@
 const video = document.querySelector('.video');
 const link = video.querySelector('.video__link');
+const image = video.querySelector('.video__image');
 const button = video.querySelector('.video__button');
 
 const createIframe = () => {
   const iframe = document.createElement('iframe');
 
   iframe.classList.add('video__media');
+  iframe.setAttribute('width', image.width);
+  iframe.setAttribute('height', image.height);
   iframe.setAttribute('allowfullscreen', '');
   iframe.setAttribute('allow', 'autoplay');
   iframe.setAttribute('src', `${link.dataset.href}?autoplay=1`);
