5916ee35e71cc8f7b3b961f03d4f6492e6ea7bd9
Ксения <easyghoul@gmail.com>
2024-04-30 13:30:58 +0700

    feat: Добавляет функционал включения/отключения слайдера на разных экранах

--- source/js/sliders/advantages-slider.js
+++ source/js/sliders/advantages-slider.js
@@ -2,44 +2,58 @@ import Swiper from 'swiper';
 import { Navigation, A11y } from 'swiper/modules';
 import 'swiper/css';
 
+let slider = null;
 const DESKTOP_WIDTH = 1440;
 
-new Swiper('#advantages-slider', {
-  modules: [Navigation, A11y],
-  loop: true,
-  speed: 500,
-  slidesPerView: 'auto',
-  spaceBetween: 30,
-  initialSlide: 3,
-  centeredSlides: true,
-  slidesPerGroup: 2,
-  observer: true,
-  observeParents: true,
+const initSlider = () => {
+  if (!slider) {
+    slider = new Swiper('#advantages-slider', {
+      modules: [Navigation, A11y],
+      loop: true,
+      speed: 500,
+      slidesPerView: 'auto',
+      spaceBetween: 30,
+      initialSlide: 3,
+      centeredSlides: true,
+      slidesPerGroup: 2,
+      observer: true,
+      observeParents: true,
 
-  navigation: {
-    nextEl: '#advantages-slider-button-next',
-    prevEl: '#advantages-slider-button-prev',
-  },
+      navigation: {
+        nextEl: '#advantages-slider-button-next',
+        prevEl: '#advantages-slider-button-prev',
+      },
 
-  a11y: {
-    enabled: true,
-    containerMessage: 'Слайдер с преимуществами.',
-    itemRoleDescriptionMessage: 'Слайд.',
-    firstSlideMessage: 'Первый слайд.',
-    lastSlideMessage: 'Последний слайд.',
-    prevSlideMessage: 'Предыдущий слайд.',
-    nextSlideMessage: 'Следующий слайд.',
-  },
+      a11y: {
+        enabled: true,
+        containerMessage: 'Слайдер с преимуществами.',
+        itemRoleDescriptionMessage: 'Слайд.',
+        firstSlideMessage: 'Первый слайд.',
+        lastSlideMessage: 'Последний слайд.',
+        prevSlideMessage: 'Предыдущий слайд.',
+        nextSlideMessage: 'Следующий слайд.',
+      },
+    });
+  }
+};
 
-  on: {
-    resize: function renderSlider(swiper) {
-      if (window.innerWidth < DESKTOP_WIDTH) {
-        swiper.disable();
-        swiper.destroy(true, true);
-      } else {
-        swiper.enable();
-        swiper.init();
-      }
-    }
+const destroySlider = () => {
+  if (slider) {
+    slider.destroy();
+    slider = null;
+  }
+};
+
+document.addEventListener('DOMContentLoaded', () => {
+  if (window.innerWidth >= DESKTOP_WIDTH) {
+    initSlider();
+  }
+});
+
+window.addEventListener('resize', () => {
+  if (window.innerWidth >= DESKTOP_WIDTH) {
+    initSlider();
+  } else {
+    destroySlider();
   }
 });
