086844242dd3d926ac27ac1b0e04f99b58280e3c
Ксения <easyghoul@gmail.com>
2024-04-30 13:31:16 +0700

    feat: Добавляет функционал слайдера gallery

--- source/js/main.js
+++ source/js/main.js
@@ -3,3 +3,4 @@ import './sliders/tours-slider';
 import './sliders/instructors-slider';
 import './sliders/reviews-slider';
 import './sliders/advantages-slider';
+import './sliders/gallery-slider';
