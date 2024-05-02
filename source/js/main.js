7a1a101683482cfb7169c8d8b6fe7c1b33a84b3a
Ксения <easyghoul@gmail.com>
2024-05-02 16:24:11 +0700

    feat: Импортирует функционал навигации в начало модуля

--- source/js/main.js
+++ source/js/main.js
@@ -1,7 +1,7 @@
+import './navigation/navigation';
 import './sliders/hero-slider';
 import './sliders/tours-slider';
 import './sliders/instructors-slider';
 import './sliders/reviews-slider';
 import './sliders/advantages-slider';
 import './sliders/gallery-slider';
-import './navigation/navigation';
