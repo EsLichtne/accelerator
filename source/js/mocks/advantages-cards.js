3ac5acd26b08840180c291dcde461c504c55aadf
Ксения <easyghoul@gmail.com>
2024-05-03 19:23:46 +0700

    feat: Добавляет моковые данные слайдов advantages

---
+++ source/js/mocks/advantages-cards.js
@@ -0,0 +1,51 @@
+const advantagesCards = [
+  `<li class="advantages-slider__item swiper-slide">
+    <article class="advantages-slider__slide advantages-card advantages-card--safety">
+      <h3>Безопасность</h3>
+      <p>Уделяем внимание безопасности клиентов, внедряя
+        строгие процедуры и&nbsp;протоколы безопасности. Имеем планы действий на&nbsp;случай непредвиденных
+        обстоятельств
+        и&nbsp;даём гарантию полной безопасности во&nbsp;время всего тура.</p>
+    </article>
+  </li>`,
+  `<li class="advantages-slider__item swiper-slide">
+    <article class="advantages-slider__slide advantages-card advantages-card--expertise">
+      <h3>Экспертиза и знания</h3>
+      <p>Наша команда опытных гидов имеет обширные
+        знания
+        о местности, погодных условиях и технике безопасности. Это&nbsp;гарантирует, что&nbsp;наши клиенты
+        находятся
+        в&nbsp;надёжных руках и могут в&nbsp;полной мере насладиться опытом без&nbsp;опасений.</p>
+    </article>
+  </li>`,
+  `<li class="advantages-slider__item swiper-slide">
+    <article class="advantages-slider__slide advantages-card advantages-card--equipment">
+      <h3>Оборудование и снаряжение</h3>
+      <p>Мы предоставляем клиентам все&nbsp;необходимое
+        оборудование, необходимое для путешествия. Это&nbsp;избавляет клиентов от&nbsp;необходимости покупать,
+        брать с&nbsp;собой
+        снаряжение, что&nbsp;делает путешествие более удобным и&nbsp;экономически выгодным.</p>
+    </article>
+  </li>`,
+  `<li class="advantages-slider__item swiper-slide">
+    <article class="advantages-slider__slide advantages-card advantages-card--routes">
+      <h3>Индивидуальные маршруты</h3>
+      <p>Предлагаем широкий выбор туров, отвечающих
+        различным уровням мастерства и интересам. Мы&nbsp;также&nbsp;можем адаптировать маршруты к вашим
+        конкретным
+        потребностям и&nbsp;предпочтениям, чтобы&nbsp;вы&nbsp;получили индивидуальный опыт.</p>
+    </article>
+  </li>`,
+  `<li class="advantages-slider__item swiper-slide">
+    <article class="advantages-slider__slide advantages-card advantages-card--nature">
+      <h3>Бережное отношение<br> к природе</h3>
+      <p>Уделяем первостепенное внимание охране
+        окружающей
+        среды и гарантируем, что туры оказывают минимальное воздействие на природную среду. Наша компания
+        следует
+        принципу «Не оставлять следов».</p>
+    </article>
+  </li>`,
+];
+
+export { advantagesCards };
