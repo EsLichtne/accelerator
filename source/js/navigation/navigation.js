ee4825f17db8dbde1d9c20cc9993c08ac3b6962e
Ксения <easyghoul@gmail.com>
2024-05-09 11:38:16 +0700

    perf: Оптимизирует код с помощью удаления обработчиков

--- source/js/navigation/navigation.js
+++ source/js/navigation/navigation.js
@@ -4,16 +4,32 @@ const button = header.querySelector('.header__button');
 const clue = button.querySelector('.header__clue');
 const content = document.querySelector('.page__content');
 
+const onHeaderClick = (event) => {
+  if (event.target.classList.contains('navigation__link')) {
+    button.click();
+  }
+};
+
+const onDocumentClick = (event) => {
+  if (header.classList.contains('header--with-menu') && !header.contains(event.target)) {
+    button.click();
+  }
+};
+
 const openMenu = () => {
   header.classList.add('header--with-menu');
   header.classList.add('page__header--with-menu');
   clue.textContent = 'Закрыть меню.';
+  header.addEventListener('click', onHeaderClick);
+  document.addEventListener('click', onDocumentClick);
 };
 
 const closeMenu = () => {
   header.classList.remove('header--with-menu');
   header.classList.remove('page__header--with-menu');
   clue.textContent = 'Открыть меню.';
+  header.removeEventListener('click', onHeaderClick);
+  document.removeEventListener('click', onDocumentClick);
 };
 
 const toggleMenu = () => {
@@ -38,18 +54,6 @@ setContentIndent();
 
 button.addEventListener('click', () => {
   toggleMenu();
-});
-
-header.addEventListener('click', (event) => {
-  if (event.target.classList.contains('navigation__link')) {
-    closeMenu();
-  }
-});
-
-document.addEventListener('click', (event) => {
-  if (header.classList.contains('header--with-menu') && !header.contains(event.target)) {
-    closeMenu();
-  }
 });
 
 export { setContentIndent };
