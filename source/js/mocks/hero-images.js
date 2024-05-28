02ea04525c6163fdc611ea91a65bc57e4042a43e
Ксения <easyghoul@gmail.com>
2024-05-28 15:03:12 +0700

    feat: Добавляет моковые данные для изображений hero

---
+++ source/js/mocks/hero-images.js
@@ -0,0 +1,22 @@
+const heroImages = [
+  {
+    'src': 'img/hero-1.jpg',
+    'srcset': 'img/hero-1@2x.jpg 2x',
+    'source': 'img/hero-1.webp 1x, img/hero-1@2x.webp 2x',
+    'alt': 'Кудрявая девушка с папками и документами с улыбкой на фоне учебного заведения.',
+  },
+  {
+    'src': 'img/hero-2.jpg',
+    'srcset': 'img/hero-2@2x.jpg 2x',
+    'source': 'img/hero-2.webp 1x, img/hero-2@2x.webp 2x',
+    'alt': 'Компания молодых людей в комнате общежития.',
+  },
+  {
+    'src': 'img/hero-3.jpg',
+    'srcset': 'img/hero-3@2x.jpg 2x',
+    'source': 'img/hero-3.webp 1x, img/hero-3@2x.webp 2x',
+    'alt': 'Молодые профессионалы в офисе за работой.',
+  },
+];
+
+export { heroImages };
